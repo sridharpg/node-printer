f6c74d50dc7ce9050b4d9a37a2d8dbc05be7fc84
Sridhar <sridhar.pg@zohocorp.com>
2019-12-29 07:07:22 +0530

    Fix windows compilation errors in node 13

--- src/node_printer_win.cc
+++ src/node_printer_win.cc
@@ -205,7 +205,7 @@ namespace{
         return result;
     }
 
-    void parseJobObject(JOB_INFO_2W *job, v8::Handle<v8::Object> result_printer_job)
+    void parseJobObject(JOB_INFO_2W *job, v8::Local<v8::Object> result_printer_job)
     {
         MY_NODE_MODULE_ISOLATE_DECL
         //Common fields
@@ -308,7 +308,7 @@ namespace{
 
     std::string retrieveAndParseJobs(const LPWSTR iPrinterName,
                                      const DWORD& iTotalJobs,
-                                     v8::Handle<v8::Object> result_printer_jobs,
+                                     v8::Local<v8::Object> result_printer_jobs,
                                      PrinterHandle& iPrinterHandle)
     {
         MY_NODE_MODULE_ISOLATE_DECL
@@ -345,7 +345,7 @@ namespace{
         return std::string("");
     }
 
-    std::string parsePrinterInfo(const PRINTER_INFO_2W *printer, v8::Handle<v8::Object> result_printer, PrinterHandle& iPrinterHandle)
+    std::string parsePrinterInfo(const PRINTER_INFO_2W *printer, v8::Local<v8::Object> result_printer, PrinterHandle& iPrinterHandle)
     {
         MY_NODE_MODULE_ISOLATE_DECL
     #define ADD_V8_STRING_PROPERTY(name, key) ADD_V8_STRING_PROPERTY2(name, key,)
@@ -688,7 +688,7 @@ MY_NODE_MODULE_CALLBACK(PrintDirect)
     }
 
     std::string data;
-    v8::Handle<v8::Value> arg0(iArgs[0]);
+    v8::Local<v8::Value> arg0(iArgs[0]);
     if (!getStringOrBufferFromV8Value(arg0, data))
     {
         RETURN_EXCEPTION_STR("Argument 0 must be a string or Buffer");
